80e85f423c766dd313c1dded0b6eb82dbb2fd3b4
kaia24 <86907898+kaia24@users.noreply.github.com>
2023-05-31 18:04:15 -0700

    Add files via upload

---
+++ NestedDictLists/nested_dict_lists.py
@@ -0,0 +1,44 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+x[1][0]=15
+students[0]['last_name']= 'Bryant'
+sports_directory['soccer'][0] = 'Andres'
+
+z[0]['y']=30
+def iterateDictionary(some_list):
+    print(some_list)
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+iterateDictionary(students)
+
+def iterateDictionary2(some_list,key):
+    for i in range(len(some_list)):
+        print(some_list[i][key])
+
+iterateDictionary2(students,'first_name')
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    print(some_dict['locations'])
+    print(some_dict['instructors'])
+
+
+printInfo(dojo)
